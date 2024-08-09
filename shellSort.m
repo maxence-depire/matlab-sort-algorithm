06d3a52f051d8fb0ca562eb5615af55ae322a27b
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-08-09 14:06:28 +0200

    Shell sort working.

--- shellSort.m
+++ shellSort.m
@@ -7,17 +7,20 @@ function tab_sort = shellSort(tab)
         pas = round(3*pas+1);
     end
 
-    while pas ~= 2
+    while pas ~= 1
         pas = round(pas/3);
         for i = pas+1:n
-            valeur = tab(n);
+            valeur = tab(i);
             j = i;
             while j > pas && tab(j-pas) > valeur
+                temp = tab(j);
                 tab(j) = tab(j-pas);
+                tab(j-pas) = temp;
                 j = j-pas;
             end
             tab(j) = valeur;
         end
     end
+
     tab_sort = tab;
 end
